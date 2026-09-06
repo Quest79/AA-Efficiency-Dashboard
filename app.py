e33c78c6b4d9b78c412d8fa26c1b1bf131f0e958
Paul <paul.nicolezim@gmail.com>
2026-09-05 18:09:54 -0700

    Bump dashboard version to 1.0.5

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from urllib.parse import urlparse
 from urllib.request import urlopen
 
 APP_NAME = "AAEfficiencyDashboard"
-VERSION = "1.0.4"
+VERSION = "1.0.5"
 
 def resource_dir() -> Path:
     if getattr(sys, "frozen", False):
