a965a8d24455af3f1819680e49d013f65f286875
Paul <paul.nicolezim@gmail.com>
2026-09-22 13:57:28 -0700

    Bump dashboard server to v1.1.8

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ from urllib.parse import urlparse
 from urllib.request import Request, urlopen
 
 APP_NAME = "AAEfficiencyDashboard"
-VERSION = "1.1.7"
+VERSION = "1.1.8"
 
 def resource_dir() -> Path:
     if getattr(sys, "frozen", False):
